756b49751bb401810c14f677bd21c18be3f5931c
Leonel Alegre <78889336+LeonelAlegreDev@users.noreply.github.com>
2024-08-05 17:56:42 -0300

    Se añadio el parametro status al constructor de la clase PublicError Se modifijo ErrorCatcher.js para que responda con el codigo status del objeto PublicError

--- errors/ErrorCatcher.js
+++ errors/ErrorCatcher.js
@@ -4,8 +4,8 @@ class ErrorCatcher {
     constructor() {}
     static async CatchError(error, res) {
         if (error instanceof PublicError) {
-            if(error.data) res.status(400).json({ message: error.message , data: error.data});
-            else res.status(400).json({ message: error.message });
+            if(error.data) res.status(error.status).json({ message: error.message , data: error.data});
+            else res.status(error.status).json({ message: error.message });
         }
         else res.status(500).json({ message: 'Internal server error' });
     }
--- errors/PublicError.js
+++ errors/PublicError.js
@@ -1,8 +1,9 @@
 class PublicError extends Error {
-    constructor(message, data) {
-      super(message);
-      this.name = 'PublicError'; // Set the error name for identification
-      this.data = data ? data : null;
-    }
+  constructor(message, data, status) {
+    super(message);
+    this.name = 'PublicError'; // Set the error name for identification
+    this.data = data ? data : null;
+    this.status = status ? status : 400;
+  }
 }
 module.exports = { PublicError };
